f6fcfc85ce194df78f36cc97a803e7c01c685f76
David <david.meth@hotmail.com>
2019-05-21 12:10:06 -0300

    lesson 25

--- LARA-LAB/case_1/app/Http/Controllers/MyCtrl.php
+++ LARA-LAB/case_1/app/Http/Controllers/MyCtrl.php
@@ -1,12 +1,17 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
 
 class MyCtrl extends Controller
 {
     public function getNome(){
         return "Jose da Silva \n";
+    }
+    public function multiplicar($n1, $n2){
+        return $n1 * $n2." \n";
+    }
+    public function getNomeByID($id){
+        return array('David','Brenda','Murilo')[$id]." \n";
     }
 }
--- LARA-LAB/case_1/routes/web.php
+++ LARA-LAB/case_1/routes/web.php
@@ -6,3 +6,7 @@ use Illuminate\Http\Request;
 //include("session_routes.php");
 
 Route::get('/nome','MyCtrl@getNome');
+
+Route::get('/multiplicar/{n1}/{n2}','MyCtrl@multiplicar');
+
+Route::get('/nome-id/{id}','MyCtrl@getNomeByID')->where('id','[0-2]');
