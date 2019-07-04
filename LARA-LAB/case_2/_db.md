a6447ce7bd5e908163ecd25bd64dc6102a240408
David <david.meth@hotmail.com>
2019-07-04 10:50:29 -0300

    lesson 57

--- LARA-LAB/case_2/_db.md
+++ LARA-LAB/case_2/_db.md
@@ -8,5 +8,9 @@
 
 **DB::table('cat')->get()**
 **DB::table('cat')->pluck('nome')** retorna campo especificado
+**DB::table('cat')->where('nome','like','%p%')->get()**
+**DB::table('cat')->where('id',1)->orWhere('id',2)->get()**
+**DB::table('cat')->whereBetween('id',[1,2])->get()**
+**DB::table('cat')->whereNotBetween('id',[1,2])->get()**
 
 <hr>
--- LARA-LAB/case_2/routes/web.php
+++ LARA-LAB/case_2/routes/web.php
@@ -16,5 +16,22 @@ Route::get("categorias",function(){
     $cats = DB::table('categorias')->where('id',1)->first();
     print_r($cats);
     echo "<hr>";
-
+    $cats = DB::table('categorias')->where('nome','like','%p%')->get();
+    print_r($cats);
+    echo "<hr>";
+    $cats = DB::table('categorias')->where('id',1)->orWhere('id',2)->get();
+    print_r($cats);
+    echo "<hr>";
+    $cats = DB::table('categorias')->whereBetween('id',[1,2])->get();
+    print_r($cats);
+    echo "<hr>";
+    $cats = DB::table('categorias')->whereNotBetween('id',[1,2])->get();
+    print_r($cats);
+    echo "<hr>";
+    $cats = DB::table('categorias')->whereIn('id',[1,3])->get();
+    print_r($cats);
+    echo "<hr>";
+    $cats = DB::table('categorias')->whereNotIn('id',[1,3,4])->get();
+    print_r($cats);
+    echo "<hr>";
 });
