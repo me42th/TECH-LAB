0ae11c7ac0b4b97888392cf93cc8b2aed070c4df
David <david.meth@hotmail.com>
2019-07-04 09:54:37 -0300

    lesson 56

--- LARA-LAB/case_2/routes/web.php
+++ LARA-LAB/case_2/routes/web.php
@@ -13,6 +13,8 @@ Route::get("categorias",function(){
     $cats = DB::table('categorias')->pluck('nome');
     print_r($cats);
     echo "<hr>";
-
+    $cats = DB::table('categorias')->where('id',1)->first();
+    print_r($cats);
+    echo "<hr>";
 
 });
