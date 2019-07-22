4e42f38c05dfef5cf29513805720205189f64233
David Meth <david.meth@hotmail.com>
2019-07-22 17:12:55 -0300

    sunday like an icecream

--- IONIC-LAB/rocky/src/app/app.component.ts
+++ IONIC-LAB/rocky/src/app/app.component.ts
@@ -4,6 +4,9 @@ import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
+import { ActivatedRoute } from '@angular/router';
+import { RecipeService } from '../recipe.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
--- IONIC-LAB/rocky/src/app/recipe/recipe.model.ts
+++ IONIC-LAB/rocky/src/app/recipe/recipe.model.ts
@@ -1,5 +1,4 @@
 export interface Recipe{
-   
     id: string;
     title: string;
     imgURL: string;
