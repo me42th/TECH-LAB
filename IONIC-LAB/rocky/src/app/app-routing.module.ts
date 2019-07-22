ea40c6c4a80cd7bd0745bde4dca9764a0cefe4b7
David Meth <david.meth@hotmail.com>
2019-07-22 09:27:04 -0300

    sunday isn ice cream

--- IONIC-LAB/rocky/src/app/app-routing.module.ts
+++ IONIC-LAB/rocky/src/app/app-routing.module.ts
@@ -4,7 +4,15 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'recipe', pathMatch: 'full' },
 
-  { path: 'recipe', loadChildren: './recipe/recipe.module#RecipePageModule' },
+  { 
+    path: 'recipe', 
+    children: [
+      {path: '', loadChildren: './recipe/recipe.module#RecipePageModule'},
+      {path: ':recipeid', loadChildren: './recipe/detail/detail.module#DetailPageModule' }
+    ]
+     
+  }
+  //{ path: 'detail',  },
 ];
 
 @NgModule({
