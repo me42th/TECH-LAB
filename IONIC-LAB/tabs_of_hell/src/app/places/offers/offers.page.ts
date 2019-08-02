dadb42f587c0b59e2eef2e86ad11b5c23a5b3969
David Meth <david.meth@hotmail.com>
2019-08-02 10:59:27 -0300

    router

--- IONIC-LAB/tabs_of_hell/src/app/places/offers/offers.page.ts
+++ IONIC-LAB/tabs_of_hell/src/app/places/offers/offers.page.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Places } from "../places.model";
+import { ActivatedRoute } from "@angular/router";
+import { NavController } from "@ionic/angular";
+import { PlacesService } from '../places.service';
 
 @Component({
-  selector: 'app-offers',
-  templateUrl: './offers.page.html',
-  styleUrls: ['./offers.page.scss'],
+  selector: "app-offers",
+  templateUrl: "./offers.page.html",
+  styleUrls: ["./offers.page.scss"]
 })
 export class OffersPage implements OnInit {
-
-  constructor() { }
+  place: Places;
+  constructor(
+    private route: ActivatedRoute, 
+    private navCtrl: NavController,
+    private placesSrv: PlacesService) {}
 
   ngOnInit() {
+    this.route.paramMap.subscribe(paramMap => {
+      if (!paramMap.has("placeid")) {
+        this.navCtrl.navigateBack("/places/tabs/offers");
+        return;
+      }
+      this.place = this.placesSrv.getPlace(paramMap.get('placeid')); 
+    });
   }
-
 }
--- IONIC-LAB/tabs_of_hell/src/app/places/places.service.ts
+++ IONIC-LAB/tabs_of_hell/src/app/places/places.service.ts
@@ -1,38 +1,39 @@
-import { Injectable, OnInit } from '@angular/core';
-import { Places } from './places.model';
+import { Injectable, OnInit } from "@angular/core";
+import { Places } from "./places.model";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class PlacesService implements OnInit {
-  private _places: Places[] =  [
+  private _places: Places[] = [
     new Places(
-      'p1',
-      'Primeiro Lugar',
-      'Um Lugar Legal',
-      'https://picsum.photos/500/200',
+      "p1",
+      "Primeiro Lugar",
+      "Um Lugar Legal",
+      "https://picsum.photos/501/201",
       9
     ),
     new Places(
-      'p2',
-      'Segundo Lugar',
-      'Outro Lugar Legal',
-      'https://picsum.photos/500/200',
+      "p2",
+      "Segundo Lugar",
+      "Outro Lugar Legal",
+      "https://picsum.photos/502/202",
       18
     ),
     new Places(
-      'p3',
-      'Terceiro Lugar',
-      'Mais Um Lugar Legal',
-      'https://picsum.photos/500/200',
+      "p3",
+      "Terceiro Lugar",
+      "Mais Um Lugar Legal",
+      "https://picsum.photos/500/200",
       27
     )
   ];
-  constructor() { }
-  get places(){
-    return [... this._places];
+  constructor() {}
+  get places() {
+    return [...this._places];
   }
-  ngOnInit(){
-   
+  getPlace(id: string) {
+    return { ...this._places.find(p => p.id === id) };
   }
+  ngOnInit() {}
 }
