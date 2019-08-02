e9a47c54c949954906c46cef9bf9796177aaf0cd
David Meth <david.meth@hotmail.com>
2019-08-02 08:49:16 -0300

    ionitem with link

--- IONIC-LAB/tabs_of_hell/src/app/places/places.service.ts
+++ IONIC-LAB/tabs_of_hell/src/app/places/places.service.ts
@@ -5,34 +5,34 @@ import { Places } from './places.model';
   providedIn: 'root'
 })
 export class PlacesService implements OnInit {
-  private _places: Places[];
+  private _places: Places[] =  [
+    new Places(
+      'p1',
+      'Primeiro Lugar',
+      'Um Lugar Legal',
+      'https://picsum.photos/500/200',
+      9
+    ),
+    new Places(
+      'p2',
+      'Segundo Lugar',
+      'Outro Lugar Legal',
+      'https://picsum.photos/501/201',
+      18
+    ),
+    new Places(
+      'p3',
+      'Terceiro Lugar',
+      'Mais Um Lugar Legal',
+      'https://picsum.photos/502/302',
+      27
+    )
+  ];
   constructor() { }
   get places(){
     return [... this._places];
   }
   ngOnInit(){
-    this._places = [
-      new Places(
-        'p1',
-        'Primeiro Lugar',
-        'Um Lugar Legal',
-        'https://picsum.photos/500/500',
-        9
-      ),
-      new Places(
-        'p2',
-        'Segundo Lugar',
-        'Outro Lugar Legal',
-        'https://picsum.photos/500/500',
-        18
-      ),
-      new Places(
-        'p3',
-        'Terceiro Lugar',
-        'Mais Um Lugar Legal',
-        'https://picsum.photos/500/500',
-        27
-      )
-    ];
+   
   }
 }
