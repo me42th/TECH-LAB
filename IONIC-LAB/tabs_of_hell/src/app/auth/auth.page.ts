7a082bb7c066510b2632cc7d0e6a9faf3915e868
David Meth <david.meth@hotmail.com>
2019-08-05 15:08:19 -0300

    creating auth guard

--- IONIC-LAB/tabs_of_hell/src/app/auth/auth.page.ts
+++ IONIC-LAB/tabs_of_hell/src/app/auth/auth.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-auth',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuthPage implements OnInit {
 
-  constructor() { }
+  constructor( private authSRV: AuthService) { }
 
   ngOnInit() {
+  }
+
+  onLogin(){
+    this.authSRV.login();
   }
 
 }
