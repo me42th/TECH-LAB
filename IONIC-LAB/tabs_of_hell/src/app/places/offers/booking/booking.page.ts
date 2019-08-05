7e0fac528c02a3cb27fdc6a61aab5aa6d0300781
David Meth <david.meth@hotmail.com>
2019-08-05 09:05:20 -0300

    scary js

--- IONIC-LAB/tabs_of_hell/src/app/places/offers/booking/booking.page.ts
+++ IONIC-LAB/tabs_of_hell/src/app/places/offers/booking/booking.page.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Places } from "../../places.model";
+import { ActivatedRoute } from "@angular/router";
+import { NavController } from "@ionic/angular";
+import { PlacesService } from '../../places.service';
+
 @Component({
   selector: 'app-booking',
   templateUrl: './booking.page.html',
   styleUrls: ['./booking.page.scss'],
 })
-export class BookingPage implements OnInit {
-
-  constructor() { }
+export class BookingPage  implements OnInit {
+  place: Places;
+  constructor(
+    private route: ActivatedRoute, 
+    private navCtrl: NavController,
+    private placesSrv: PlacesService) {}
 
   ngOnInit() {
+    
+    this.route.paramMap.subscribe(paramMap => {
+    
+          
+    if (!paramMap.has("placeid")) {
+        this.navCtrl.navigateBack('/places/tabs/offers');
+        return;
+      }
+      
+      this.place = this.placesSrv.getPlace(paramMap.get('placeid')); 
+      console.log(this.place);
+      
+        }); 
   }
-
 }
--- IONIC-LAB/tabs_of_hell/src/app/places/offers/offers.page.ts
+++ IONIC-LAB/tabs_of_hell/src/app/places/offers/offers.page.ts
@@ -9,20 +9,6 @@ import { PlacesService } from '../places.service';
   templateUrl: "./offers.page.html",
   styleUrls: ["./offers.page.scss"]
 })
-export class OffersPage implements OnInit {
-  place: Places;
-  constructor(
-    private route: ActivatedRoute, 
-    private navCtrl: NavController,
-    private placesSrv: PlacesService) {}
+export class OffersPage  {
 
-  ngOnInit() {
-    this.route.paramMap.subscribe(paramMap => {
-      if (!paramMap.has("placeid")) {
-        this.navCtrl.navigateBack("/places/tabs/offers");
-        return;
-      }
-      this.place = this.placesSrv.getPlace(paramMap.get('placeid')); 
-    });
-  }
 }
