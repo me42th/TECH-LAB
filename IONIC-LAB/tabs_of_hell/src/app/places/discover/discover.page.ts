e3bf6843b906773258d4d1cfb2b13715156c80ad
David Meth <david.meth@hotmail.com>
2019-08-08 10:56:02 -0300

    lesson 131

--- IONIC-LAB/tabs_of_hell/src/app/places/discover/discover.page.ts
+++ IONIC-LAB/tabs_of_hell/src/app/places/discover/discover.page.ts
@@ -1,23 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { PlacesService } from '../places.service';
 import { Places } from '../places.model';
-import { MenuController } from '@ionic/angular';
+import { MenuController, IonItemSliding } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-discover',
   templateUrl: './discover.page.html',
   styleUrls: ['./discover.page.scss'],
 })
+
 export class DiscoverPage implements OnInit {
   places: Places[];
   constructor(
     private placesSRV: PlacesService,
-    private menuCTRL: MenuController
+    private menuCTRL: MenuController,
+    private router: Router
     ) {  }
 
   ngOnInit(){
     this.places = this.placesSRV.places;
   }
+
+  onEdit(func: string, id: string, sliding: IonItemSliding){
+    sliding.close();
+    console.log(func+' | '+id+' G12');
+    this.router.navigate(['/','places','tabs','discover',id]);
+
+  }
+
 
   onOpenMenu(){
     this.menuCTRL.toggle();
