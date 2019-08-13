bd1cd55048dd5391c7c7ac4d4da1af9ad557f289
David Meth <david.meth@hotmail.com>
2019-08-13 13:43:05 -0300

    Section 2 Lesson 33

--- IONIC-LAB/section_two/ng-refresh/src/app/PersonsComponent/persons-input.component.ts
+++ IONIC-LAB/section_two/ng-refresh/src/app/PersonsComponent/persons-input.component.ts
@@ -1,17 +1,23 @@
-import { Component, Output, EventEmitter } from '@angular/core';
-
+import { Component} from '@angular/core';
+import { PersonsService } from './persons.service';
 
 @Component({
   selector: 'persons-input',
   templateUrl: './persons-input.component.html',
   styleUrls: ['./persons-input.component.css']
 })
+
 export class PersonsInputComponent{
-  @Output() personCreate = new EventEmitter<string>();
+
   email: string = '';
   otherEmail: string = '';
+
+  constructor(
+    private prsService: PersonsService
+  ){}
+
   onCreatePerson(name: string){
-    this.personCreate.emit(name);
     console.log(name + '|' + this.email + '|' + this.otherEmail);
+    this.prsService.addPerson(name);
   }
 }
