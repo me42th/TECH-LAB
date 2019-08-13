bc4b376ceb1fdd130f295156a10f03f9987907e2
David Meth <david.meth@hotmail.com>
2019-08-13 10:03:06 -0300

    Section 2 Lesson 23

---
+++ IONIC-LAB/section_two/ng-refresh/_readme.md
@@ -0,0 +1,3 @@
+# RUN FOREST, RUN
+
+## ng serve
---
+++ IONIC-LAB/section_two/ng-refresh/src/app/PersonsComponent/persons.component.ts
@@ -0,0 +1,9 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-person',
+  templateUrl: './persons.component.html'
+})
+export class PersonsComponent{
+  @Input() personsList: string[];
+}
