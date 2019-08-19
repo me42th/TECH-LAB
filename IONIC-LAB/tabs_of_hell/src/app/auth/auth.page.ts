3eeb17ace45b56244b11fddae7e7565cf28cc53d
David Meth <david.meth@hotmail.com>
2019-08-19 16:08:19 -0300

    section 10 lesson 163

--- IONIC-LAB/tabs_of_hell/src/app/auth/auth.page.ts
+++ IONIC-LAB/tabs_of_hell/src/app/auth/auth.page.ts
@@ -23,7 +23,16 @@ export class AuthPage implements OnInit {
   }
 
   onSubmit(form: NgForm) {
-    console.log(form);
+    if(!form.valid){
+      return;
+    }
+    const email = form.value.email;
+    const password = form.value.password;
+    console.log(email+' '+password);
+    if(this.isLogin)
+      console.log('login');
+    else
+      console.log('signup');  
   }
 
   onSwitchAuthMode(){
