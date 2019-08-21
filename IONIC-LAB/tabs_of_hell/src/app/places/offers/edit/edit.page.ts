7cbaa68933f5a7cc938aaf38ac8bc200ef43f705
David Meth <david.meth@hotmail.com>
2019-08-21 10:38:36 -0300

    section 10 lesson 171

--- IONIC-LAB/tabs_of_hell/src/app/places/offers/edit/edit.page.ts
+++ IONIC-LAB/tabs_of_hell/src/app/places/offers/edit/edit.page.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { NavController } from '@ionic/angular';
+
+import { PlacesService } from '../../places.service';
+import { Places } from '../../places.model';
+import { FormGroup, FormControl, Validators  } from '@angular/forms';
 
 @Component({
   selector: 'app-edit',
@@ -7,9 +13,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditPage implements OnInit {
 
-  constructor() { }
+  place: Places;
+  form: FormGroup;
 
+  constructor(
+    private route: ActivatedRoute,
+    private placesSRV: PlacesService,
+    private navCTRL: NavController 
+  ) { }
+    onUpdateOffer(){
+      if (!this.form.valid){
+        return;
+      }
+
+      console.log('click');
+    }
   ngOnInit() {
+    this.route.paramMap.subscribe(paramMap => {
+      if (!paramMap.has('placeid')){
+        this.navCTRL.navigateBack('/places/tabs/offers');
+        return;
+      }
+      this.place = this.placesSRV.getPlace(paramMap.get('placeid'));
+      this.form = new FormGroup({
+        title: new FormControl(
+          this.place.title, {
+            updateOn: 'blur',
+            validators: [Validators.required]
+          }
+        ) ,
+        description: new FormControl(
+          this.place.description, {
+            updateOn: 'blur',
+            validators: [Validators.required, Validators.maxLength(180)]
+          })
+      });
+    });
   }
 
 }
