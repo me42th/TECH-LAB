90d73efee902b2eabfed12fadc42f2ffb644b3be
David Meth <david.meth@hotmail.com>
2019-09-06 11:42:42 -0300

    lesson 166

--- IONIC-LAB/forms_of_metal/src/app/form-group/form-group.page.ts
+++ IONIC-LAB/forms_of_metal/src/app/form-group/form-group.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form-group',
@@ -7,12 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormGroupPage implements OnInit {
 
+  form: FormGroup;
   constructor() { }
+
   onCreate() {
     console.log('go johnny, go');
     alert('hit the road jack');
   }
-  ngOnInit() {
 
+  ngOnInit() {
+    this.form = new FormGroup({
+      titleInput: new FormControl('Valor', {
+        updateOn: 'blur',
+        validators: [Validators.required]
+       }),
+      shortTextArea: new FormControl(null, {
+        updateOn: 'blur',
+        validators: [Validators.required, Validators.maxLength(120)]
+       }),
+      numberInput: new FormControl(null, {
+        updateOn: 'blur',
+        validators: [Validators.required, Validators.min(1)]
+       }),
+      fromDate: new FormControl(null, { 
+        updateOn: 'blur',
+        validators: [Validators.required]
+      }),
+      toDate: new FormControl(null, { 
+        updateOn: 'blur',
+        validators: [Validators.required]
+      })
+    });
   }
 }
