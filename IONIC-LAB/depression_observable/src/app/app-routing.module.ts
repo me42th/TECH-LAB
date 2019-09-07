7fe5ee487944a3c6ca6dd85ef26f747cad07b6a9
David Meth <david.meth@hotmail.com>
2019-09-07 10:41:06 -0300

    observadores

--- IONIC-LAB/depression_observable/src/app/app-routing.module.ts
+++ IONIC-LAB/depression_observable/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
   { path: 'observable-hello-world', loadChildren: './observable-hello-world/observable-hello-world.module#ObservableHelloWorldPageModule' },
+  { path: 'dummy', loadChildren: './dummy/dummy.module#DummyPageModule' },
 ];
 
 @NgModule({
--- IONIC-LAB/depression_observable/src/app/observable-hello-world/observable-hello-world.page.ts
+++ IONIC-LAB/depression_observable/src/app/observable-hello-world/observable-hello-world.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { map, take } from "rxjs/operators";
+import { map, take, skipLast, skip } from "rxjs/operators";
 import { interval, pipe } from "rxjs";
 
 @Component({
@@ -9,22 +9,77 @@ import { interval, pipe } from "rxjs";
 })
 export class ObservableHelloWorldPage implements OnInit {
 
-  obsVar: string = ' |';
+  var0 = ' ';
+  var1 = ' ';
+  var2 = ' ';
+  var3 = ' ';
+  var4 = ' ';
+  var5 = ' ';
+  
   constructor() { }
 
   ngOnInit() {
-    
   }
 
-  onClick(){
-   
+  onClick() {
+    console.log('a');
     interval(1000).pipe(
-      take(100),
+     take(12),
+     skipLast(10),
+     map(
+      v => Date.now()
+     )
+    ).subscribe(
+      value => this.var0 = this.var0 + value + ' | '
+    );
+    interval(1000).pipe(
+      take(12),
+      skip(2),
+      skipLast(8),
       map(
         v => Date.now()
       )
     ).subscribe(
-      value => this.obsVar = this.obsVar + value + ' |'
+       value => this.var1 = this.var1 + value + ' | '
+    );
+    interval(1000).pipe(
+      take(12),
+      skip(4),
+      skipLast(6),
+      map(
+        v => Date.now()
+      )
+    ).subscribe(
+      value => this.var2 = this.var2 + value + ' | '
+    );
+    interval(1000).pipe(
+      take(12),
+      skip(6),
+      skipLast(4),
+      map( 
+        v => Date.now()
+      )
+    ).subscribe(
+      value => this.var3 = this.var3 + value + ' | '
+    );
+    interval(1000).pipe(
+      take(12),
+      skip(8),
+      skipLast(2),
+      map( 
+        v => Date.now()
+      )
+    ).subscribe(
+      value => this.var4 = this.var4 + value + ' | '
+    );
+    interval(1000).pipe(
+      take(12),
+      skip(10),
+      map(
+        v => Date.now()
+      )
+    ).subscribe(
+      value => this.var5 = this.var5 + value + ' | '
     );
   }
 
