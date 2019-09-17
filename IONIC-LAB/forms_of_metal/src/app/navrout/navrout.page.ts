7249ec23c10bb77de8c750d8738e1a7e4f2bd588
David Meth <david.meth@hotmail.com>
2019-09-17 16:02:10 -0300

    its sad, go back to navctrl

--- IONIC-LAB/forms_of_metal/src/app/navrout/navrout.page.ts
+++ IONIC-LAB/forms_of_metal/src/app/navrout/navrout.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TuxComponent } from './tux/tux.component';
 
 @Component({
   selector: 'app-navrout',
@@ -8,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 export class NavroutPage implements OnInit {
 
   constructor() { }
-
+  root: TuxComponent;
   ngOnInit() {
   }
 
--- IONIC-LAB/forms_of_metal/src/app/navrout/tux/tux.component.ts
+++ IONIC-LAB/forms_of_metal/src/app/navrout/tux/tux.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tux.component.scss'],
 })
 export class TuxComponent implements OnInit {
-
+  
   constructor() { }
 
   ngOnInit() {}
