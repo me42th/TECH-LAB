5e9d88ce6f56ff947fdac113e2f7ea047124d438
David Meth <david.meth@hotmail.com>
2019-09-23 14:41:56 -0300

    localstorage

--- IONIC-LAB/storage_lab/src/app/db.service.ts
+++ IONIC-LAB/storage_lab/src/app/db.service.ts
@@ -1,9 +1,21 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable } from '@angular/core';
+import { LOCAL_STORAGE, StorageService } from 'ngx-webstorage-service';
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class DbService {
+  // key that is used to access the data in local storageconst
+  key = 'local_todolist';
 
-  constructor() { }
+  constructor(@Inject(this.key) private storage: StorageService) { }
+
+  store(chave: string, valor: string){
+    const currentTodoList = this.storage.get(this.key) || [];
+    currentTodoList.push({chave, valor});
+    this.storage.set(this.key, currentTodoList);
+    console.log(this.storage.get(this.key) || 'LocaL storage is empty');
+  }
 }
