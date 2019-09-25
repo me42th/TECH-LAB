011283e2b1210dff817ff0d4ddcdb302e1d4ddb0
David Meth <david.meth@hotmail.com>
2019-09-25 11:40:25 -0300

    run ios, run

---
+++ IONIC-LAB/_iosfck.md
@@ -0,0 +1,24 @@
+# IOS 
+
+## LINKS
+
+DOC - https://capacitor.ionicframework.com/docs/ios
+
+## FILE
+
+> ./capacitor.config.json
+
+## SHELL
+
+```
+- [X] xcode-select --install
+- [X] ionic capacitor add ios
+- [X] ionic capacitor copy ios
+- [X] ionic capacitor run ios
+```
+
+## XCODE
+
+### SIGNING & CAPABILITIES
+
+> ADD A TEAM
--- IONIC-LAB/storage_lab/src/app/home/home.page.ts
+++ IONIC-LAB/storage_lab/src/app/home/home.page.ts
@@ -2,20 +2,21 @@ import { Component } from '@angular/core';
 import { DbService } from '../db.service';
 import { FormControl, FormGroup } from '@angular/forms';
 
-
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
 
   nomeInput: FormControl = new FormControl(null, {updateOn: 'change'});
-
   lista: any[];
+
   form: FormGroup = new FormGroup({
     nome : this.nomeInput
   });
+
   constructor(private db: DbService) {
     this.db.observable
     .subscribe( value => this.lista = value);
@@ -30,4 +31,5 @@ export class HomePage {
   onDel(value: string) {
     this.db.del(value);
   }
+
 }
