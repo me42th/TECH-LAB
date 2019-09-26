2241a8efcd09675acfdfc597621efc95105f5a99
David Meth <david.meth@hotmail.com>
2019-09-26 12:22:16 -0300

    push

--- IONIC-LAB/push_lab/src/app/app.component.ts
+++ IONIC-LAB/push_lab/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 
-import { Platform } from '@ionic/angular';
+import { Platform, AlertController } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { OneSignal } from '@ionic-native/onesignal/ngx';
+
 
 @Component({
   selector: 'app-root',
@@ -15,7 +16,8 @@ export class AppComponent {
     private platform: Platform,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar,
-    private signal: OneSignal
+    private signal: OneSignal,
+    private alertCTRL: AlertController
   ) {
     this.initializeApp();
   }
@@ -25,5 +27,37 @@ export class AppComponent {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
+  }
+
+  async showAlert( title, msg, task) {
+    const alert = await this.alertCTRL.create({
+      header: title,
+      subHeader: msg,
+      buttons: [
+        {text: `Action: ${task}`, handler: () => {}}
+      ]
+    });
+    alert.present();
+  }
+
+  setuPush() {
+    this.signal.startInit(/*ONE SIGNAL CODE*/, /*GOOGLE CODE*/);
+
+    this.signal.inFocusDisplaying(this.signal.OSInFocusDisplayOption.Notification);
+
+    this.signal.handleNotificationReceived().subscribe(data => {
+      const msg = data.payload.body;
+      const title = data.payload.title;
+      const addtional = data.payload.additionalData;
+      this.showAlert(title, msg, addtional.task);
+    });
+
+    this.signal.handleNotificationOpened().subscribe(data =>  {
+      const additionalData = data.notification.payload.additionalData;
+      this.showAlert('Notificações','Você leu isso', additionalData.task);
+    });
+
+
+    this.signal.endInit();
   }
 }
