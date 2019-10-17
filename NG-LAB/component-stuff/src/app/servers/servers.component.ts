7850c7790ab228053b8248355fae287c00ff6f1d
me42th <david.meth@hotmail.com>
2019-10-17 16:55:27 -0300

    lgLesson 26

--- NG-LAB/component-stuff/src/app/servers/servers.component.ts
+++ NG-LAB/component-stuff/src/app/servers/servers.component.ts
@@ -1,8 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: '[app-servers]',
   template: `
+  <button class="btn btn-primary"
+  [disabled]="allowNewServer"
+  (click)="flag()">Add Server</button>
   <div class="app-server"></div>
   <div class="app-server"></div>
   <div class="app-server"></div>
@@ -11,8 +15,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
   variavel = 'teste';
-  constructor() { }
-
+  allowNewServer = false;
+  constructor() {
+    interval(2000).subscribe(
+      () => {
+        if (this.allowNewServer) {
+          this.allowNewServer = false;
+        }
+      }
+   );
+  }
+  flag() {
+    this.allowNewServer = !this.allowNewServer;
+  }
   ngOnInit() {
   }
 
