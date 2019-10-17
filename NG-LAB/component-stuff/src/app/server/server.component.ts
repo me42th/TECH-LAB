fb2b64368ccbf8b51e13ddf4eff0c50722928ae3
me42th <david.meth@hotmail.com>
2019-10-17 16:38:57 -0300

    ngLesson 25

--- NG-LAB/component-stuff/src/app/server/server.component.ts
+++ NG-LAB/component-stuff/src/app/server/server.component.ts
@@ -2,8 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: '.app-server',
-  templateUrl: './server.component.html',
+  //templateUrl: './server.component.html'
+  template: '<p>Server with ID {{serverID}} IS {{getServerStatus()}} </p>',
 })
 export class ServerComponent {
-
+ serverID: number = 10;
+ serverStatus: 'offline' | 'online' = 'offline';
+ getServerStatus() {
+   return this.serverStatus;
+ }
 }
