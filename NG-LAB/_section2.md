b83c65f1caf03a6eea668c0e293fa1f2b205c79c
me42th <david.meth@hotmail.com>
2019-10-22 10:17:01 -0300

    ngLesson 38

--- NG-LAB/_section2.md
+++ NG-LAB/_section2.md
@@ -2,6 +2,26 @@
 
 <details>
   <summary>SHELL</summary>
-    ng generate component [NAME]
-    ng g c [NAME]
+    <p>ng generate component [NAME]</p>
+    <p>ng g c [NAME]</p>
 </details>  
+
+## NGSTYLE
+> SETA VALORES DINAMICOS AO ELEMENTO
+
+```
+@Component({
+  selector: '.app-server',
+  //templateUrl: './server.component.html'
+  template: `
+  <p
+  [ngStyle]="{
+    backgroundColor: color()
+  }"
+  >Server with ID {{id}} IS {{serverStatus}}
+  </p>`,
+})
+```
+
+## NGCLASS
+> ADD OU REMOVE UMA CLASSE DADO UMA CONDIÇÃO
--- NG-LAB/component-stuff/src/app/server/server.component.ts
+++ NG-LAB/component-stuff/src/app/server/server.component.ts
@@ -3,12 +3,37 @@ import { Component } from '@angular/core';
 @Component({
   selector: '.app-server',
   //templateUrl: './server.component.html'
-  template: '<p>Server with ID {{serverID}} IS {{getServerStatus()}} </p>',
+  template: `
+  <p
+  [ngStyle]="{
+    backgroundColor: color()
+  }"
+  [ngClass]="{
+    online: serverStatus === 'online'
+  }"
+  >Server with ID {{id}} IS {{serverStatus}}
+  </p>`,
+  styles: [`
+  .online{
+    color: white
+  }
+  `]
 })
 export class ServerComponent {
- serverID: number = 10;
- serverStatus: 'offline' | 'online' = 'offline';
- getServerStatus() {
-   return this.serverStatus;
+ static serverID: number = 1;
+ private _id: number = ServerComponent.serverID++;
+ private _serverStatus: 'offline' | 'online' = 'offline';
+
+ color() {
+   return this.serverStatus === 'online' ? 'green' : 'red';
+ }
+ constructor() {
+   this._serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+ }
+ get id() {
+   return this._id;
+ }
+ get serverStatus() {
+   return this._serverStatus;
  }
 }
