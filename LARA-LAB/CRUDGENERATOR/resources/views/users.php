4eb2dd50bb96f22bc5b3153b53b9975ca47c2923
me42th <david.meth@hotmail.com>
2020-02-08 13:53:03 -0300

    axios+crudgenerator+logviewer

---
+++ LARA-LAB/CRUDGENERATOR/resources/views/users.php
@@ -0,0 +1,28 @@
+<script src="http://unpkg.com/vue"></script>
+<script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+
+<div id="app">
+<ul>
+  <li v-for="pessoa in info.data.data">
+    {{ pessoa.name }}
+  </li>
+</ul>
+</div>
+<script>
+new Vue({
+  el: '#app',
+  data () {
+    return {
+      info: null
+    }
+  },
+  mounted () {
+    setTimeout(
+      () =>  axios
+              .get('http://localhost:8000/api/users')
+              .then(response => (this.info = response))
+      ,200
+    )
+  }
+})
+</script>
