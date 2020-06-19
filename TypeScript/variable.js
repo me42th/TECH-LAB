7366c1cd98f1e6fd75f05ad01347612198de3ec3
me42th <david.meth@hotmail.com>
2020-06-19 09:27:13 -0300

    19-06-2020 let vs var

--- TypeScript/variable.js
+++ TypeScript/variable.js
@@ -1,15 +1,11 @@
 someFN();
-function someFN()
-{
-    if(true)
-    {
+var y = 200;
+function someFN() {
+    if (true) {
         var x = 100;
-        let y = 200;
         console.log(x);
-        console.log(y);
-
     }
     console.log(x);
     console.log(y);
 }
-
+console.log(y);
