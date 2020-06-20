8ca2bfa3d11a18ffb76244f04abceefbc7a3f5b6
me42th <david.meth@hotmail.com>
2020-06-20 13:07:09 -0300

    lesson 5

---
+++ TypeScript/operators.js
@@ -0,0 +1,8 @@
+var x = 4;
+var y = 2;
+console.log('Soma ' + x + y);
+console.log('Subtracao ' + (x - y));
+console.log('Multiplicacao ' + (x * y));
+console.log('Divisao ' + (x / y));
+console.log('Modulo ' + (x % y));
+console.log('Potencia ' + (Math.pow(x, y)));
---
+++ TypeScript/operators.ts
@@ -0,0 +1,9 @@
+var x = 4;
+var y = 2;
+
+console.log('Soma '+x+y);
+console.log('Subtracao '+(x-y));
+console.log('Multiplicacao '+(x*y));
+console.log('Divisao '+(x/y));
+console.log('Modulo '+(x%y));
+console.log('Potencia '+(x**y));
