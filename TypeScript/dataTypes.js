c358099973fda52ee863701c7c25eae80238a29f
me42th <david.meth@hotmail.com>
2020-06-20 00:06:00 -0300

    19-06-2020 lesson 4

---
+++ TypeScript/dataTypes.js
@@ -0,0 +1,34 @@
+// Number
+var decimalNumber = 12.00;
+var hexaNumber = 0xdeadcafe;
+var octaNumber = 255;
+var binaryNumber = 170;
+console.log(decimalNumber);
+console.log(hexaNumber);
+console.log(octaNumber);
+console.log(binaryNumber);
+// String
+var nameDev = "David";
+console.log(nameDev + " Meth");
+// Boolean
+var flag = true;
+console.log(flag);
+// Void
+function hello() {
+    console.log('This is welcome msg');
+}
+;
+// Null
+var nula = null;
+// Fixo
+var fixada = 'ABC';
+// Undefined
+var indefinido = undefined;
+// Any Type
+var val = 'Hi';
+function consol(x, y) {
+    console.log(x + y);
+}
+;
+consol('David', 'Brenda');
+consol(1, 2);
