210972880191e7d155cab91c762136d57c3cc5da
me42th <david.meth@hotmail.com>
2020-06-21 16:57:18 -0300

    lesson 6

--- TypeScript/string.js
+++ TypeScript/string.js
@@ -1,2 +1,14 @@
 var mystring = "Welcome to TypeScript";
-var otherstring = "other stuff";
+var otherstring = "                             My Brother                   ";
+console.log(mystring.charAt(0));
+console.log(otherstring);
+console.log(mystring);
+console.log(otherstring = ' ' + otherstring.trimRight());
+console.log(mystring = mystring.concat(otherstring));
+console.log(mystring = mystring.replace('Type', 'Java'));
+console.log(mystring.split(' '));
+console.log(mystring.split(' ', 1));
+console.log(mystring.split(' ', 3));
+console.log(mystring.substring(0, 7));
+console.log(mystring.toUpperCase());
+console.log(mystring.toLowerCase());
