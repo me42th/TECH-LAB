1a65c3c8e4a134e3ef5f672c09c0a14d0307cfa5
me42th <david.meth@hotmail.com>
2020-06-23 20:16:44 -0300

    lesson 11

--- TypeScript/function.js
+++ TypeScript/function.js
@@ -13,6 +13,25 @@ var num = function (x, y) {
 function optional(x) {
     console.log(x);
 }
+var arrow = function (x) { return console.log(x); };
+var defaultValue = function (x) {
+    if (x === void 0) { x = 'default'; }
+    return console.log(x);
+};
+function restArgs() {
+    var name = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        name[_i] = arguments[_i];
+    }
+    console.log(name);
+    return name.join(' ');
+}
 optional();
 optional(null);
 display(num(10, 20));
+defaultValue();
+defaultValue('other');
+console.log(restArgs('1'));
+console.log(restArgs('1', '2'));
+console.log(restArgs('1', '2', '3'));
+console.log(restArgs('1', '2', '3', '4'));
