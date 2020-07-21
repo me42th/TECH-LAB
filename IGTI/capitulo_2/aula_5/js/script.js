4883acad3fe96561566cfc0c67b834e37ef0bc91
me42th <david.meth@hotmail.com>
2020-07-20 23:36:26 -0300

    Capitulo 2 Aula 10

--- IGTI/capitulo_2/aula_5/js/script.js
+++ IGTI/capitulo_2/aula_5/js/script.js
@@ -1,21 +1,46 @@
 const dbNames = [];
+var form = null;
+var input = null;
+var div = null;
+
 var pos = 0;
+
 window.addEventListener(
     'load',
     () => {
-        const form = document.querySelector('form');
-        const input = document.querySelector('#inputName');
-        
+        form = document.querySelector('form');
+        input = document.querySelector('#inputName');
+        div = document.querySelector('#names');
+
         form.addEventListener(
             'submit',
             (event) => {
                 event.preventDefault();
                 dbNames.push(input.value);
                 input.value = '';
-                alert(dbNames);
+                render();
+            }
+        );
+
+        input.addEventListener(
+            'keyup',
+            (event) => {
             }
         );
         
         input.focus();
     }
 );
+
+function render(){
+    div.innerHTML = '';
+    var ul = document.createElement('ul');
+    dbNames.forEach(
+        (element) => {
+            let li = document.createElement('li');
+            li.textContent = element;
+            ul.appendChild(li);
+        }
+    );
+    div.appendChild(ul);
+}
