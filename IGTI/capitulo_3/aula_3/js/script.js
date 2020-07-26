72096ae161650d025d8377e1273ff28cfae1f64c
me42th <david.meth@hotmail.com>
2020-07-26 16:24:55 -0300

    Aula 11 B

--- IGTI/capitulo_3/aula_3/js/script.js
+++ IGTI/capitulo_3/aula_3/js/script.js
@@ -1,15 +1,25 @@
 window.addEventListener(
     'load',
     () => {
-        mapedPeople = mapPeople();
+        mapedPeople = forEachPeople();
+        
         console.log(mapedPeople);
-        console.log(filterPeople('David'));
+        console.log(filterPeople('Minas Gerais'));
+        console.log(doReduce());
+        console.log(findPeople('David'));
     }
 );
 var mapedPeople;
 
+const findPeople = (firstName) => {
+    return people.results.find(
+        person => person.name.first === firstName 
+    );
+}
+
 const mapPeople = () => {
-    return people.results.map( 
+    let baianos = filterPeople('Bahia');
+    return baianos.map( 
         person => {
             return {
                 name: `${person.name.first} ${person.name.last}`,
@@ -19,8 +29,24 @@ const mapPeople = () => {
     );
 }
 
-const filterPeople = (firstName) => {
+const forEachPeople = () => {
+    let arrayOfPeople = mapPeople();
+
+    arrayOfPeople.forEach( 
+        (person,key) => 
+        person.id = key
+    );
+    return arrayOfPeople;
+}
+
+const doReduce = () => {
+    return mapedPeople.reduce(
+        (acumulator,current) => acumulator+current.name.length+current.email.length,0 
+    );
+}
+
+const filterPeople = (state) => {
     return people.results.filter(
-        person => person.name.first === firstName
+        person => person.location.state === state
     );
 }
