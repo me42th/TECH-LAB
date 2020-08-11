76d9abaa20d01ed0ecd56b1cd29297090ded9ce5
me42th <david.meth@hotmail.com>
2020-08-11 10:55:20 -0300

    lesson 20

--- LARA-LAB/udemy_58/section_5/app/Http/Controllers/MeuControlador.php
+++ LARA-LAB/udemy_58/section_5/app/Http/Controllers/MeuControlador.php
@@ -9,4 +9,27 @@ class MeuControlador extends Controller
     public function index(Request $request){
         return json_encode($request);
     }
+
+    public function produtos(){
+        echo "
+        <h1>Produtos</h1>
+        <ol>
+            <li>Notebook</li>
+            <li>Impressora</li>
+            <li>Mouse</li>
+        </ol>
+    ";
+    }
+
+    public function getIdade(){
+        return '30 anos';
+    }
+
+    public function getNome(){
+        return 'Jose da Silva';
+    }
+
+    public function multiplicar($numeroX, $numeroY){
+        return $numeroX * $numeroY;
+    }
 }
