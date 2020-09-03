2d8c3183c7f18a4bc8634cf816f92521f4beef36
me42th <david.meth@hotmail.com>
2020-09-03 11:24:55 -0300

    03_09 lesson 15

--- LARA-LAB/review/section_5/routes/web.php
+++ LARA-LAB/review/section_5/routes/web.php
@@ -18,16 +18,45 @@ Route::get('/', function () {
 });
 
 
-Route::get('/ola/{nome}/{sobrenome?}',
-    function($nome, $sobrenome = ''){
+Route::get(
+    '/ola/{nome}/{sobrenome?}',
+    function ($nome, $sobrenome = '') {
         return "Olá $nome $sobrenome seja bem vindo!";
     }
 );
 
-Route::get('/regra/{palavra}/{numero}',
-    function($palavra,$numero){
-        for($cont = 0;$cont < $numero;$cont++){
+Route::get(
+    '/regra/{palavra}/{numero}',
+    function ($palavra, $numero) {
+        for ($cont = 0; $cont < $numero; $cont++) {
             echo $palavra;
             echo "<br>";
         }
-    })->where('palavra','[A-Za-z]+')->where('numero','[0-9]+');
+    }
+)->where('palavra', '[A-Za-z]+')->where('numero', '[0-9]+');
+
+Route::prefix('/agrupamento')->group(
+    function(){
+        Route::get('/',
+            function(){
+                return view('root');
+            }
+        );
+        Route::get('/user',
+            function(){
+                return view('user');
+            }
+        );
+        Route::get('/profile',
+            function(){
+                return view('profile');
+            }
+        );
+        Route::get('/{valor}',
+            function($valor){
+                return "Não implementado";
+            }
+        );
+    }
+
+);
