f566e2ba02418f8fd97f4c4c6e81a874376fad9a
me42th <david.meth@hotmail.com>
2020-09-05 18:33:41 -0300

    05_09 lesson 37

--- LARA-LAB/review/section_7/resources/views/numbers/index.blade.php
+++ LARA-LAB/review/section_7/resources/views/numbers/index.blade.php
@@ -1,4 +1,9 @@
 @extends('layouts.principal')
+@section('titulo','Numbers')
 @section('conteudo')
 <h1>Numeros</h1>
+@for($i = 0 ; $i <= 50; $i++)
+    {{$i}}
+@endfor
+<hr>
 @endsection
--- LARA-LAB/review/section_7/resources/views/vogais/index.blade.php
+++ LARA-LAB/review/section_7/resources/views/vogais/index.blade.php
@@ -1,4 +1,13 @@
 @extends('layouts.principal')
+@section('titulo','Vogais')
 @section('conteudo')
 <h1>Vogais</h1>
+@php
+    $vogais = ['a','e','i','o','u'];
+@endphp
+@foreach ($vogais as $vogal)
+    <p>
+        {{$vogal}} | Index {{$loop->index}} | Count {{$loop->count}} | Iteration {{$loop->iteration}}  @if($loop->first) | First @endif  @if($loop->last) | Last @endif
+    </p>
+@endforeach
 @endsection
