d0d542059d8e031159fd76b2edf57b58635ff844
me42th <david.meth@hotmail.com>
2020-09-08 11:09:22 -0300

    08_09 lesson 70

--- LARA-LAB/review/section_10/resources/views/layout/app.blade.php
+++ LARA-LAB/review/section_10/resources/views/layout/app.blade.php
@@ -11,13 +11,20 @@
         body {
             padding: 20px;
         }
+        .navbar{
+            margin-bottom: 20px;
+        }
     </style>
 </head>
 <body>
     <div class="container">
-        @hasSection ('body')
-            @yield('body')
-        @endif
+        @component('component.navbar')
+        @endcomponent
+        <main role="main">
+            @hasSection ('body')
+                @yield('body')
+            @endif
+        </main>
     </div>
     <script src="{{asset('js/app.js')}}" type="text/javascript"></script>
 </body>
