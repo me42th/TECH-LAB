726979257d478a33e56cefdeb4f5e655d25cd69d
me42th <david.meth@hotmail.com>
2020-09-08 10:53:09 -0300

    08_09 lesson 69

--- LARA-LAB/review/section_10/resources/views/layout/app.blade.php
+++ LARA-LAB/review/section_10/resources/views/layout/app.blade.php
@@ -3,10 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">]
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{csrf_token()}}">
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
     <title>Cadastro de Produtos</title>
+    <style>
+        body {
+            padding: 20px;
+        }
+    </style>
 </head>
 <body>
     <div class="container">
