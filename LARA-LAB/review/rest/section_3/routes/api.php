59a6238b6f5d12b33c7606d701054fce15fb4a99
me42th <david.meth@hotmail.com>
2020-09-16 09:24:43 -0300

    16_09 rest lesson 10++

--- LARA-LAB/review/rest/section_3/routes/api.php
+++ LARA-LAB/review/rest/section_3/routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Http\Response;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,4 +16,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('test',function(Request $request){
+    dd($request->headers->all());
+    $response = new Response(json_encode(['msg' => 'Api']));
+    $response->header('Content-Type','application/json');
+    $response->header('David','Meth');
+    return $response;
 });
