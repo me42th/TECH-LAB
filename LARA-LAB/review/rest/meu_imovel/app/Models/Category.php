68c07af60b0ce06394be04a2e992b9fa3f06a282
me42th <david.meth@hotmail.com>
2020-10-26 08:15:12 -0300

    26/10/2020 lesson 48

--- LARA-LAB/review/rest/meu_imovel/app/Models/Category.php
+++ LARA-LAB/review/rest/meu_imovel/app/Models/Category.php
@@ -14,4 +14,9 @@ class Category extends Model
         'description',
         'slug'
     ];
+
+    public function realStates(){
+        return $this->belongsToMany(RealState::class);
+    }
+
 }
