15323513453fd67a1ba549ed9fb18af9a93213fe
me42th <david.meth@hotmail.com>
2020-12-01 13:46:50 -0300

    [ALURA] lesson 1

--- NODE-LAB/api-petshop/api/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const config = require('config')
-const app = express()
-
-app.use(bodyParser.json())
-
-app.listen(3000, () => console.log(config))
