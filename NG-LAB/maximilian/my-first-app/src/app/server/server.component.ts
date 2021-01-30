7dbacacc5b01b58d521e2b919b00efa558e8a44d
me42th <david.meth@live.com>
2021-01-30 19:04:26 -0300

    lesson 22

--- NG-LAB/maximilian/my-first-app/src/app/server/server.component.ts
+++ NG-LAB/maximilian/my-first-app/src/app/server/server.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 @Component({
-  selector:"app-server",
+  selector:"[app-server]",
   templateUrl:"./server.component.html",
   styles: [
     `h3 {
--- NG-LAB/maximilian/my-first-app/src/app/servers/servers.component.ts
+++ NG-LAB/maximilian/my-first-app/src/app/servers/servers.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
+  selector: '.app-servers',
   template: `
-    <app-server></app-server>
-    <app-server></app-server>
+    <div app-server></div>
+    <div app-server></div>
   `,
   styleUrls: ['./servers.component.css']
 })
