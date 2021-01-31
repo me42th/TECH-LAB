a0b5a7f7293ec7597f4aa744a5014a5c4c7d71a3
me42th <david.meth@live.com>
2021-01-31 17:45:32 -0300

    pratice 1

---
+++ NG-LAB/maximilian/pratice/src/app/warning/warning.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+  template: `
+    <div class="alert alert-warning" role="alert">
+      Manually Component
+    </div>
+  `,
+  selector: 'app-warning'
+})
+export class WarningComponent{}
