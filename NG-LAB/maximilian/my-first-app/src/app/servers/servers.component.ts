e06f45d2249d7b0199ddb2f95b2d4f25109a7442
me42th <david.meth@live.com>
2021-02-06 12:15:57 -0300

    lesson 37

--- NG-LAB/maximilian/my-first-app/src/app/servers/servers.component.ts
+++ NG-LAB/maximilian/my-first-app/src/app/servers/servers.component.ts
@@ -10,11 +10,13 @@ export class ServersComponent {
   isAllowedNewServer = false;
   serverCreationStatus = 'No server was created!';
   serverName = '';
+  serverCreated = false;
   constructor() {
     setTimeout(() => this.isAllowedNewServer = true, 2000);
   }
 
   onCreateServer(){
+    this.serverCreated = true;
     this.serverCreationStatus = 'Server '+this.serverName+' created';
   }
   onUpdateServer(event: any){
