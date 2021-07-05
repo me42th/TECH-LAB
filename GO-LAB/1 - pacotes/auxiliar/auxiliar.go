57b1a013ec1debf91e5f00b8ef0661ed58a5d127
me42th <david.meth@hotmail.com>
2021-07-05 09:54:25 -0300

    lesson 5

--- GO-LAB/1 - pacotes/auxiliar/auxiliar.go
+++ GO-LAB/1 - pacotes/auxiliar/auxiliar.go
@@ -4,4 +4,5 @@ import "fmt"
 
 func Escrever() {
 	fmt.Println("Auxiliar")
+	escrever2()
 }
--- GO-LAB/1 - pacotes/main.go
+++ GO-LAB/1 - pacotes/main.go
@@ -3,9 +3,13 @@ package main
 import (
 	"fmt"
 	"modulo/auxiliar"
+
+	"github.com/badoux/checkmail"
 )
 
 func main() {
 	fmt.Println("main")
 	auxiliar.Escrever()
+	erro := checkmail.ValidateFormat("email@abc.com")
+	fmt.Println(erro)
 }
