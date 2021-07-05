3cadcca26a318715667868119f905696f94b868f
me42th <david.meth@hotmail.com>
2021-07-05 17:51:11 -0300

    lesson 9

--- GO-LAB/1 - Fundamentos/main.go
+++ GO-LAB/1 - Fundamentos/main.go
@@ -1,11 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"modulo/variaveis"
-)
+import "modulo/variaveis"
 
 func main() {
-	v1, v2 := variaveis.Funcoes(1, 2)
-	fmt.Println(v1, v2)
+	variaveis.Operadores()
 }
